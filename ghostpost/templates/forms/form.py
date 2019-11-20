662bb689986f3409bcc12f594b0ce8d745530226
ethan375 <ethanebel1@gmail.com>
2019-11-20 00:54:22 -0600

    new post from rendering correctly

--- ghostpost/templates/forms/form.py
+++ ghostpost/templates/forms/form.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class NewPost(forms.Form):
-    boast = forms.CheckboxInput()
-    post = forms.CharField(widget=forms.Textarea(max_length=280))
+    boast = forms.BooleanField()
+    post = forms.CharField(widget=forms.Textarea, max_length=280)
