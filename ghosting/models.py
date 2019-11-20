b408f49cce882c73a50e62fa0580eb2939fe2a27
ethan375 <ethanebel1@gmail.com>
2019-11-20 00:21:36 -0600

    created post model

--- ghosting/models.py
+++ ghosting/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    boast = models.BooleanField()
+    post = models.CharField(max_length=280)
+    up_votes = models.IntegerField()
+    down_votes = models.IntegerField()
+    creation_time = models.DateTimeField()
